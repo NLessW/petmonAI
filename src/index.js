3540caad7c1d3060a469bb2f87305d1cc05d02ef
NLessW <qkqkhih55@gmail.com>
2026-02-10 17:48:20 +0900

    해상도 변경

--- src/index.js
+++ src/index.js
@@ -70,7 +70,7 @@ function displayClassLabels() {
 document.getElementById('start-webcam-btn').addEventListener('click', async () => {
     try {
         webcamStream = await navigator.mediaDevices.getUserMedia({
-            video: { width: 640, height: 360, facingMode: 'environment' },
+            video: { width: 640, height: 180, facingMode: 'environment' },
         });
         document.getElementById('webcam').srcObject = webcamStream;
         document.getElementById('start-test-btn').disabled = false;
