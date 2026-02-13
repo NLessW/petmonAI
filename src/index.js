04bba751c631ac56a9df653b5b8cfea808ff61f6
NLessW <qkqkhih55@gmail.com>
2026-02-13 16:48:24 +0900

    해상도

--- src/index.js
+++ src/index.js
@@ -70,7 +70,7 @@ function displayClassLabels() {
 document.getElementById('start-webcam-btn').addEventListener('click', async () => {
     try {
         webcamStream = await navigator.mediaDevices.getUserMedia({
-            video: { width: 480, height: 480, facingMode: 'environment' },
+            video: { width: 640, height: 640, facingMode: 'environment' },
         });
         document.getElementById('webcam').srcObject = webcamStream;
         document.getElementById('start-test-btn').disabled = false;
